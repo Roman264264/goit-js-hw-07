769e460193a3865594470e795eefc7719b9b4fb0
Roman_Lubennikov <lubennikovroman@gmail.com>
2025-08-21 21:04:00 +0300

    js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const body = document.querySelector("body");
+const colorSpan = document.querySelector(".color");
+const btn = document.querySelector(".change-color");
+
+btn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
